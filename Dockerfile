e2728e595fbcfac46e799649fbd0eeec62739602
frazz <59382025+FrazzleDazzley@users.noreply.github.com>
2023-03-12 10:58:16 -0700

    init

---
+++ Dockerfile
@@ -0,0 +1,41 @@
+FROM python:alpine3.15
+
+ENV TZ=""
+ENV PD_API_KEY=""
+ENV PD_SERVICE=""
+ENV SLACK_API=""
+ENV SLACK_CHANNEL_NAME=""
+ENV GITHUB_API=""
+ENV GITHUB_REPO=""
+ENV CHECK_TIMER=""
+ENV PYTHONUNBUFFERED=true
+
+
+LABEL Maintainer="@frazzled_dazzle"
+LABEL Company="stkd.io"
+
+#Setup env
+RUN apk update;\
+    apk add py-pip;\
+    apk add --no-cache tzdata
+
+RUN pip install feedparser;\
+    pip install datetime;\
+    pip install time;\
+    pip install sys;\
+    pip install pathlib;\
+    pip install pdpyras;\
+    pip install os;\
+    pip install slack_sdk;\
+    pip install requests;\
+    pip install PyGithub
+
+#Patch the image
+RUN apk upgrade
+
+RUN mkdir /data;
+
+COPY ./app/main.py /data/github_alert.py
+
+CMD [ "python", "/data/github_alert.py"]
+
---
+++ app/main.py
@@ -0,0 +1,87 @@
+from curses import raw
+from datetime import datetime, timedelta
+import sys
+import os
+from os import environ
+import pdpyras
+from slack_sdk import WebClient
+import time
+from github import Github
+
+##########################
+#
+#       Variables
+#
+##########################
+#PAger Duty Variables
+if environ.get('PD_API_KEY') is not None:
+    #Service -> Custom Change Event Transformer key
+    PD_API_KEY = os.environ['PD_API_KEY']
+
+    #Setting Pager duty
+    session = pdpyras.EventsAPISession(PD_API_KEY)
+#Service to page in pager duty
+if environ.get('PD_SERVICE') is not None:
+    PD_SERVICE = os.environ['PD_SERVICE'] 
+
+
+#Slack variables
+if environ.get('SLACK_CHANNEL_NAME') is not None:
+    SLACK_CHANNEL_NAME = os.environ['SLACK_CHANNEL_NAME']
+
+if environ.get('SLACK_API') is not None:
+    SLACK_API = os.environ['SLACK_API']
+
+#Github variables
+if environ.get('GITHUB_API') is not None:
+    GITHUB_API = os.environ['GITHUB_API']    
+
+if environ.get('GITHUB_REPO') is not None:
+    GITHUB_REPO = os.environ['GITHUB_REPO'] 
+
+if environ.get('CHECK_TIMER') is not None:
+    CHECK_TIMER = os.environ['CHECK_TIMER']
+else:
+    CHECK_TIMER = 60
+
+##########################
+#
+#       functions
+#
+##########################
+def send_slack_message(api, channel_name, message):
+    client = WebClient(api)
+    response = client.chat_postMessage(
+        channel=channel_name,
+        text=message)
+
+def sendAlert(message):
+    sys.stdout.write(message)
+    if environ.get('SLACK_API') is not None and environ.get('SLACK_CHANNEL_NAME') is not None:
+        send_slack_message(SLACK_API, SLACK_CHANNEL_NAME, message)
+    if environ.get('PD_API_KEY') is not None:
+        session.trigger(message, "polkadot")
+    time.sleep(3600)
+
+##########################
+#
+#       Main
+#
+##########################
+def main():
+    G = Github(GITHUB_API) # Put your GitHub token here
+    repo = G.get_repo(GITHUB_REPO) # 'paritytech/polkadot'
+    releases = repo.get_releases()
+
+    for release in releases:
+        #Check if released in the last hour
+        if (datetime.utcnow() - datetime.strptime(str(release.published_at), '%Y-%m-%d %H:%M:%S')) < timedelta(minutes=CHECK_TIMER):
+            message = "[!] New Release: " + release.title 
+            #sendAlert(message)
+            quit()
+        
+    print("No New Release in the last " + str(CHECK_TIMER) + " minutes, lastest release was: " + releases[0].title)
+
+
+if __name__ == "__main__":
+    main()
